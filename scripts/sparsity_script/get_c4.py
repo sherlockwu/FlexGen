e84f168cfc73e2ef30b71bc3e9084e571182d5fa
Kan Wu <wukanustc@gmail.com>
2024-01-30 15:49:38 -0800

    add script to get c4 dataset samples

---
+++ scripts/sparsity_script/get_c4.py
@@ -0,0 +1,30 @@
+from datasets import load_dataset
+from tqdm import tqdm
+import json
+
+# prerequisites: pip install datasets
+
+def dump_jsonl(data, output_path, append=False):
+    """
+    Write list of objects to a JSON lines file.
+    """
+    mode = "a+" if append else "w"
+    with open(output_path, mode, encoding="utf-8") as f:
+        for line in data:
+            json_record = json.dumps(line, ensure_ascii=False)
+            f.write(json_record + "\n")
+
+
+dataset = load_dataset("c4", "en", split="train", streaming=True)
+dataset = dataset.shuffle(buffer_size=10000, seed=42)
+path = "c4_train.jsonl"
+
+for idx, doc in enumerate(tqdm(dataset)):
+    data = {
+        "max_tokens": 0,
+        "prompt": doc["text"],
+    }
+    dump_jsonl([data], path, append=True)
+    if idx == 500:
+        print("Collected 500 samples")
+        break
